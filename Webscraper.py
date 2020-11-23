dabd5472dd82584ffe9db412bfae4e119fdea7b8
Simon Shamoon <71435388+simonshm5@users.noreply.github.com>
2020-11-23 00:46:51 -0700

    Updated as GUI, but deprecated
    It is against Amazon TOS to webscrape product data, and they recommend that you utilize AWS services to do so. It seems that there have been more measures to prevent data scraping. You can read more here: https://webservices.amazon.com/paapi5/documentation/register-for-pa-api.html

--- Webscraper.py
+++ Webscraper.py
@@ -2,26 +2,64 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
+import tkinter as tk
+from tkinter import messagebox, Text
 
-URL = 'x'
 
-headers = {
-    "x'}
+root = tk.Tk()
+root.geometry("400x200")
+root.title("Amazon Price Checker by Simon Shamoon")
+
+
+user_input = tk.StringVar(root)
+
+label = tk.Label(root, text="Enter your browser's user agent")
+label.pack()
+label1 = tk.Label(root, text="Enter URL of Amazon produict")
+label1.pack()
+label2 = tk.Label(root, text="We will email you when the price falls below")
+label2.pack()
+label3 = tk.Label(root, text="Please enter your email")
+label3.pack()
+
+headers = requests.utils.default_headers()
+headers = tk.Entry(root, width = 40)
+headers.pack()
+
+url = tk.Entry(root, width = 40)
+url.pack()
+
+threshold = tk.Entry(root, width = 40)
+threshold.pack()
+
+recipient = tk.Entry(root, width = 40)
+recipient.pack()
 
 def check_price():
-    page = requests.get(URL, requests, headers=headers)
+    page = requests.get(url.get(), requests, headers={'User-Agent': headers.get().strip()})
+    soup = BeautifulSoup(page.content, 'lxml')
+    
+    try:
+        title = soup.find(id='productTitle').get_text()
+    except AttributeError:
+        title = 'null'
+        messagebox.showerror("Error", "Your item cannot currently be tracked.")
 
-    soup = BeautifulSoup(page.content, 'html.parser')
 
-    title = soup.find(id='productTitle').get_text()
-    price = soup.find(id ='priceblock_ourprice').get_text()
-    converted_price = float(price[1:10])
-
-    if (converted_price < 72.61):
-        send_mail()
-
+    try:
+        price = soup.find(id ='priceblock_ourprice').get_text()
+    except AttributeError:
+        price = 0.000000
+        messagebox.showerror("Error", "Your item cannot currently be tracked. This is probably due to Amazon blocking scripting on their site.")
+    
+    converted_price = float(price[1:4])
     print(converted_price)
     print(title.strip())
+
+    if (converted_price < int(threshold.get())):
+        send_mail()
+
+    
 
 def send_mail():
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -29,17 +67,28 @@ def send_mail():
     server.starttls()
     server.ehlo()
 
-    server.login('x@gmail.com', 'appPassword') #x@gmail.com will be the email that you want to send the email from. 
+    server.login('simonshm5@gmail.com', 'alhmrwxpcmknbztz')
 
-    subject = 'price lowered!'
-    body = 'link : https://www.amazon.com/Cooler-Master-MasterLiquid-Chamber-MLA-D24M-A18PC-R1/dp/B07CRGC899/ref=sr_1_3?crid=27DW5B6SBZO0Y&dchild=1&keywords=pc+liquid+cooler&qid=1596674717&sprefix=pc+liquid+%2Caps%2C204&sr=8-3'
+    subject = 'Your Item Has Fallen Under Your Desired Purchase Price'
+    body = 'Link :' + url.get()
     msg = f"Subject: {subject}\n\n{body}"
 
-    server.sendmail('x@gmail.com', 'y@gmail.com', msg) #x sends to y. x must be the same in line 32
+    server.sendmail('simonshm5@gmail.com', recipient.get(), msg)
     print("email has been sent")
 
     server.quit()
 
+
+sendButton = tk.Button(root, text = "Submit And Close", command = check_price)
+sendButton.pack()
+
+tk.messagebox.showwarning(title = "Deprecation Warning", message = "This project is officially deprecated. Please use Amazon's API to get item data.\nYou can see more here: https://webservices.amazon.com/paapi5/documentation/register-for-pa-api.html")
+root.mainloop()
+ 
+
+
+
 while (True):
     check_price()
     time.sleep(60*60*12)
+
